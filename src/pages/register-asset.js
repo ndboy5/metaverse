7007cb17a145c405e9a525014d46f729f7387d16
Nduka Okengwu <splashnds@yahoo.com>
2023-11-07 08:46:57 +0100

    created folder structure and startup styling for project

---
+++ src/pages/register-asset.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterAsset = () => {
+  return <div>RegisterAsset</div>;
+};
+
+export default RegisterAsset;
---
+++ src/pages/user-admin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserAdministration = () => {
+  return <div>UserAdministration</div>;
+};
+
+export default UserAdministration;
