3bc4604bb677938f27af8cb25ecba7d965a1ed1d
Nduka Okengwu <splashnds@yahoo.com>
2023-11-07 07:43:31 +0100

    installed dependencies and initialized hardhat node

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "my-metaverse-development-prj.infura-ipfs.io",
+      "ipfs.infura.io",
+      "infura-ipfs.io",
+    ],
+    formats: ["image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const testVar = { name: "ND" };
